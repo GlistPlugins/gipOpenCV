121b575d767d595386d508c3a765eff3717d0273
OnurDemir-Dev <60292029+OnurDemir-Dev@users.noreply.github.com>
2022-01-14 10:09:57 +0300

    Libary, Includes and DLL files deleted

--- prebuilts/include/zzip.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZZIP_WARNING
-#define ZZIP_WARNING 1
-#ifdef __GNUC__
-#warning do no not use <zzip.h>, update to include <zzip/lib.h>
-#else
-#error   do no not use <zzip.h>, update to include <zzip/lib.h>
-#endif
-#endif
-#include "zzip/lib.h"
